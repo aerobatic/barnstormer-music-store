4509dce7377183c4eafac978dee14fb662a756f3
David Von Lehman <david.vonlehman@gmail.com>
2013-11-27 21:50:59 -0800

    Adding data attribute

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.locals.pretty = true;
 
 app.locals.aerobatic = {
-  clientId: process.env.AEROBATIC_CLIENT_ID,
+  clientId: "sdfgsdfgsdfg", //process.env.AEROBATIC_CLIENT_ID,
   airportUrl: process.env.AEROBATIC_AIRPORT_URL || "http://localhost:3000"
 };
 
